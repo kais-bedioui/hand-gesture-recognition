0aff8a96d1d2e7be1ac4737689aa4d0bf14c7bfd
kais-bedioui <kais.bedioui@seafar.eu>
2024-11-18 22:37:22 +0100

    Cleaning trailing lines

--- src/live_demo.py
+++ src/live_demo.py
@@ -7,20 +7,6 @@ from hand_sign_recognition.hand_detector import HandDetector
 from hand_sign_recognition.hand_signs_recogniser import HandSignsRecogniser
 from utils.landmarks_visualiser import draw_landmarks_on_image
 
-FINGERS = ["Thumb", "Index", "Middle", "Ring", "Pinky"]
-HANDS = ["Right", "Left"]
-
-# One-hot-encoding of target hand signs
-# The format of the data is as follows:
-# {"hand_sign": [Thumb, Index, Middle, Ring, Pinky]}
-# True: Finger is extended, False: Finger is folded (Not Extended) 
-HAND_SIGNS = {
-    "open hand": [True, True, True, True, True],
-    "peace sign": [False, True, True, False, False],
-    "surf sign": [True, False, False, False, True],
-    "ok sign": [False, False, True, True, True]
-}
-
 def main(model_asset_path="hand_landmarker.task", num_hands=2, stream=True):
     hand_detector = HandDetector(model_asset_path=model_asset_path, num_hands=num_hands, stream=stream)
     hand_sign_recogniser = HandSignsRecogniser()
